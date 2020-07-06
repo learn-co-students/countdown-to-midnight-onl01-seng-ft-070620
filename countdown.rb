4d2f1a18723b177c8deb1ea1c2786ced0f9c0a8d
Andrew Ribas <andrew.j.ribas@gmail.com>
2020-07-06 18:17:30 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,18 @@
 #write your code here
 
-def countdown
+def countdown(num)
+  while num != 0
+    puts "#{num} Second(s)!".upcase
+    num -= 1
+  end
+  return "Happy new year!".upcase
+end
+
+def countdown_with_sleep(num)
+  while num != 0
+    puts "#{num} Second(s)!".upcase
+    num -= 1
+    sleep(1)
+  end
+  return "Happy new year!".upcase
 end
