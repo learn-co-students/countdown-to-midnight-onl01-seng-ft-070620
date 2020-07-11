546d0566698eee53ad3324ff11411590411dac24
Cameron Cousins <cameroncousins18@gmail.com>
2020-07-11 04:53:51 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,35 +1,47 @@
-
-# def countdown(seconds_left)
- # while number > 0
-  #  puts "#{seconds_left} SECONDS(S)!" 
-   # seconds_left -= 1
+#def countdown
+ # number = 10 
+#  while number > 0
+#    puts "#{number} SECOND(S)!"
+#    number -= 1
+#    sleep 1
 #  end
- # "HAPPY NEW YEAR!"
-# end
+#print "HAPPY NEW YEAR!"
+#end 
 
-
-# def countdown_with_sleep(seconds_left)
-  #  until seconds_left < 1  
-  #  puts "#{seconds_left} SECONDS(S)!" 
-   # seconds_left -= 1
-    #sleep 5
-  #end
-  #"HAPPY NEW YEAR!"
-# end
+#countdown
+#end 
+#
+#def countdown_with_sleep
+#  number = 10
+#  while number > 0
+#    puts "#{number} SECOND(S)!"
+#    number -= 1
+#    sleep 1
+#  end
+#print "HAPPY NEW YEAR!"
+#end#
+#
+#countdown_with_sleep
+#end
 
 def countdown(number)
-  while number > 0 do 
+  while number > 0
     puts "#{number} SECOND(S)!"
+    sleep(1)
     number -= 1
   end
+  # puts "HAPPY NEW YEAR!"
   "HAPPY NEW YEAR!"
+  
 end
 
-def countdown
-  def countdown_with_sleep(number)
-    while number > 0 do 
+def countdown_with_sleep(number)
+  while number > 0
     puts "#{number} SECOND(S)!"
-    sleep 1
-    number -= 1  
+    sleep(1)
+    number -= 1
   end
+  # puts "HAPPY NEW YEAR!"
+  "HAPPY NEW YEAR!"
+  
 end
